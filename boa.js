b2ff523263c54b8799542d7acae30a909bebb2c4
Filip Doversten <filip@doversten.se>
2016-12-26 12:46:24 +0100

    Commented out servo command.

--- boa.js
+++ boa.js
@@ -10,8 +10,8 @@ exec('python ./lib/Adafruit_PWM_Servo_Driver/Servo_ToolTest.py 0', function(erro
   console.log(stdout);
   console.log(stderr);
 });
-exec('python ./lib/Adafruit_PWM_Servo_Driver/Servo_ToolTest.py 1', function(error, stdout, stderr){
-  console.log(error);
-  console.log(stdout);
-  console.log(stderr);
-});
+// exec('python ./lib/Adafruit_PWM_Servo_Driver/Servo_ToolTest.py 1', function(error, stdout, stderr){
+//   console.log(error);
+//   console.log(stdout);
+//   console.log(stderr);
+// });
